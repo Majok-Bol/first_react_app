6af1e946499e5de30aaf5138a0b3b526d8f0b93b
Majok-Bol <bolmajok001@gmail.com>
2025-06-24 00:26:23 +0300

    added images

--- my_first_app/src/App.jsx
+++ my_first_app/src/App.jsx
@@ -1,43 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-import { useEffect, useState } from "react"
-
-// import './App.css'
-const Card=({title})=>{
-    // eslint-disable-next-line no-unused-vars
-    const [hasLiked,setHasLiked]=useState(false);
-    // eslint-disable-next-line no-unused-vars
-    const [count,setCount]=useState(0);
-    //use effect for fetching data
-    useEffect(()=>{
-      console.log(`${title} has been liked: ${hasLiked} `);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[hasLiked]);
-  
-  return(<div class="card" onClick={()=>{
-    setCount(count+1);
-   
-  }}><h2>{title}<br></br> -{count}</h2>
-  <button onClick={()=>setHasLiked(!hasLiked)}>{hasLiked?'❤️':'🤍'}</button>
-  </div>)
-  
-}
-
+import React from 'react'
 const App=()=>{
-
   return(
-    <div className="card-container">
-    <h2>Trending Movies</h2>
-    <Card  title="Escape From Sobibor"/>
-      <Card title="Jason Bourne"/>
-        <Card  title="Star Wars"/>
-          <Card title="Anaconda"/>
-            <Card title="Sniper"/>
-    </div>
+    <main >
+      <div className='pattern'>
 
+      </div>
+      <div className='wrapper'>
+        <header>
+        <h1>Find <span className='text-gradient'>Movies</span> you will watch without hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+
+    </main>
   )
-  
 }
 export default App
