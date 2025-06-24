364e6de62496e1e3157531e16e02766b26e29324
Majok-Bol <bolmajok001@gmail.com>
2025-06-24 14:06:45 +0300

    added search functionality

--- my_first_app/src/App.jsx
+++ my_first_app/src/App.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable no-undef */
 import React from 'react'
+import Search from './components/Search'
+import { useState } from 'react'
 const App=()=>{
+  const [searchTerm,setSearchTerm]=useState("");
   return(
     <main >
       <div className='pattern'>
@@ -7,9 +11,11 @@ const App=()=>{
       </div>
       <div className='wrapper'>
         <header>
-        <h1>Find <span className='text-gradient'>Movies</span> you will watch without hassle</h1>
+          <img src='/hero-img.png' alt="Hero Banner"/>
+        <h1>Find <span className='text-gradient'>Movies</span> You will Enjoy without hassle</h1>
         </header>
-        <p>Search</p>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      <h1 className='text-white'>{searchTerm}</h1>
       </div>
 
     </main>
